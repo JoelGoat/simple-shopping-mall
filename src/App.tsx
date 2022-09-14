9a86703c1da774bd9686ec7d9f17f56b0aab9bd5
royalbluec <tkyeom96@gmail.com>
2022-09-14 21:11:12 +0900

    상품목록 페이지 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useRoutes } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { getClient } from './queryClient'
 import { routes } from './routes'
 
 const App = () => {
   const elem = useRoutes(routes)
+  const queryClient = getClient()
 
-  return <>{elem}</>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {elem}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,25 @@
-const Products = () => <div>Products</div>
+import { useQuery } from 'react-query'
+import { fetcher, QueryKeys } from '../../queryClient'
+import { Product } from '../../types'
+import ProductItem from '../../components/product/item'
+
+const Products = () => {
+  const { data } = useQuery<Product[]>(QueryKeys.PRODUCTS, () =>
+    fetcher({
+      method: 'GET',
+      path: '/products',
+    })
+  )
+
+  return (
+    <div>
+      <ul className="products">
+        {data?.map((product) => (
+          <ProductItem {...product} key={product.id} />
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default Products
